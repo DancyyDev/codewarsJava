2564c26277daf8ff22b2f6d87f1a4dfae6a3e74f
Daniel Cha <cha.danieldev@gmail.com>
2023-01-17 14:49:41 -0500

    8kyu

---
+++ binToDecimal.java
@@ -0,0 +1,9 @@
+// Complete the function which converts a binary number (given as a string) to a decimal number.
+// 8kyu
+
+public class binToDecimal {
+    public static int convertInt(String inp){
+        // your code here
+        return Integer.parseInt(inp, 2);
+      }
+}
