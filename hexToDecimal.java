55c1ed22d0e0c3cb5a51e2a3acef890f39588ad9
Daniel Cha <cha.danieldev@gmail.com>
2023-01-18 20:28:53 -0500

    8kyu

---
+++ hexToDecimal.java
@@ -0,0 +1,10 @@
+// Complete the function which converts hex number (given as a string) to a decimal number.
+// 8kyu
+
+public class hexToDecimal {
+    public static int convertHex(final String hex) {
+        // Your code here
+            return Integer.parseInt(hex, 16);
+      }
+    
+}
