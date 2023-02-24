f613c91cf065dc5937e0822ae59c0717c6feae3e
Daniel Cha <cha.danieldev@gmail.com>
2023-02-23 23:28:42 -0500

    7kyu

--- ArithmeticFunction.java
+++ ArithmeticFunction.java
@@ -1,15 +1,15 @@
 class ArithmeticFunction {
-    public static int arithmetic(int a, int b, String op) {
-      // your code here
-      int add = a + b;
-      int subtract = a - b;
-      int multiply = a * b;
-      int divide = a / b;
-      
-      if(op.equals("add")) return add;
-      if(op.equals("subtract")) return subtract;
-      if(op.equals("multiply")) return multiply;
-      return divide;
-      
-    }
+  public static int arithmetic(int a, int b, String op) {
+    // your code here
+    int add = a + b;
+    int subtract = a - b;
+    int multiply = a * b;
+    int divide = a / b;
+    
+    if(op.equals("add")) return add;
+    if(op.equals("subtract")) return subtract;
+    if(op.equals("multiply")) return multiply;
+    return divide;
+    
   }
+}
